e0b0e1b9b7fa2dd904c3619c3e730862d60694cc
jawadh-html <j366681@gmail.com>
2025-11-18 21:42:25 -0800

    vowelss

--- vowels.go
+++ vowels.go
@@ -1,27 +1,26 @@
-package main
-
+package main 
 
 import "fmt"
 
+func main(){
 
+    var input string 
 
-func main() {
-
-    var input string
-    
-    fmt.Print("Enter a string: ")
-
+    fmt.Print("Enter a string to check how many vowels it contains: ")
     fmt.Scanln(&input)
-    
+
     count := 0
 
-    for _, char := range input {
-
-        if char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' ||
-           char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U' {
-            count++
-        }
+    for _, char := range input{
+         
+               if char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' ||
+                  char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U' {
+                  
+                  count++
+                  }
     }
-    
-    fmt.Printf("'%s' has %d vowels\n", input, count)
+
+
+            fmt.Printf("%s has %d vowels ",input , count)
+
 }
